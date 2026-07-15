730a542ce68f64627a0846987afb0fa7e7659ae2
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2026-07-14 22:46:31 -0600

    feat: :sparkles: CRUD de Pacientes con doctrine:generate-admin

---
+++ apps/frontend/modules/pacientes/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/pacientesGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/pacientesGeneratorHelper.class.php';
+
+/**
+ * pacientes actions.
+ *
+ * @package    reto_symfony
+ * @subpackage pacientes
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class pacientesActions extends autoPacientesActions
+{
+}
