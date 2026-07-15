ac8ca634ded532359d405365133cd0abe199eef3
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2026-07-15 00:00:50 -0600

    feat: :sparkles: Instalar, configurar e integrar el sistema de autenticación sfGuard.

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -2,11 +2,10 @@
 
 require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
-
+// Implementar el plugin 
 class ProjectConfiguration extends sfProjectConfiguration
 {
-  public function setup()
-  {
-    $this->enablePlugins('sfDoctrinePlugin');
+  public function setup(){
+    $this->enablePlugins('sfDoctrinePlugin', 'sfDoctrineGuardPlugin');
   }
 }
